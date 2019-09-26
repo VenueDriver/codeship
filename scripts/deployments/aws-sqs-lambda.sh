62b10476ee4670dfb674ad5c32070c8b84ef47c8
Derek Norrbom <dnorrbom@hakkasangroup.com>
2019-09-26 13:04:41 -0700

    aws sqs lambda
    deploy script for aws sqs lambda

---
+++ scripts/deployments/aws-sqs-lambda.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+aws s3 mb s3://${BUCKET_NAME}
+sam build
+sam package --template-file .aws-sam/build/template.yaml  --output-template-file ${CI_BRANCH}-packaged.yaml --s3-bucket ${BUCKET_NAME}
+sam deploy --template-file ${CI_BRANCH}-packaged.yaml --stack-name ${STACK_NAME} --capabilities CAPABILITY_IAM --parameter-overrides Token=${TOKEN} Environment=${CI_BRANCH} Host=${HOST} Stack=${STACK_NAME}
