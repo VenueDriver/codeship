083d3a49a9ebe28d216d28ab9ae68a0d28471f75
Derek Norrbom <dnorrbom@hakkasangroup.com>
2019-10-17 13:03:00 -0700

    testing
    testing variable overwrite

--- scripts/deployments/aws-sqs-lambda.sh
+++ scripts/deployments/aws-sqs-lambda.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+if [ $CI_BRANCH == "master" ]; then export CI_BRANCH="production"; fi
 aws s3 mb s3://${BUCKET_NAME}
 sam build
 sam package --template-file .aws-sam/build/template.yaml  --output-template-file ${CI_BRANCH}-packaged.yaml --s3-bucket ${BUCKET_NAME}
