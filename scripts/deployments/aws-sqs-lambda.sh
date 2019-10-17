dcfc3433429ca53bcb8855b7defc6ab2f2f89b14
Derek Norrbom <dnorrbom@hakkasangroup.com>
2019-10-17 13:08:12 -0700

    bucket
    update bucket url to include branch

--- scripts/deployments/aws-sqs-lambda.sh
+++ scripts/deployments/aws-sqs-lambda.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ $CI_BRANCH == "master" ]; then export CI_BRANCH="production"; fi
-aws s3 mb s3://${BUCKET_NAME}
+aws s3 mb s3://sqs-payments-${CI_BRANCH}.venuedriver.engineering
 sam build
 sam package --template-file .aws-sam/build/template.yaml  --output-template-file ${CI_BRANCH}-packaged.yaml --s3-bucket ${BUCKET_NAME}
 sam deploy --template-file ${CI_BRANCH}-packaged.yaml --stack-name ${STACK_NAME} --capabilities CAPABILITY_IAM --parameter-overrides Token=${TOKEN} Environment=${CI_BRANCH} Host=${HOST} Stack=${STACK_NAME}
